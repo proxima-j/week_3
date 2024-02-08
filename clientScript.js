6f99eaf73c775ff7fe12d244a2af6971a2f65cb1
proxima-j <32122209+proxima-j@users.noreply.github.com>
2024-02-07 19:38:37 -0500

    adding connection to  red node

--- clientScript.js
+++ clientScript.js
@@ -1,1 +1,14 @@
-const socket = new WebSocket("ws://localhost:1880/example")
+const socket = new WebSocket("ws://localhost:1880/testPage");
+
+// socket.addEventListener("open", handleSocketOpen);
+
+// function handleSocketOpen(){
+//     document.body.style.backgroundColor= "green"
+// }
+
+socket.addEventListener("message", handleMessage);
+
+function handleMessage(event){
+        document.body.style.backgroundColor= "green"
+        //document.getElementById("socket_data").innerHTML=event.data;
+    };
