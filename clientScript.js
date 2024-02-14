db57e17b49307710d05eafb5f2dda46a0d7f3a97
proxima-j <32122209+proxima-j@users.noreply.github.com>
2024-02-14 04:48:25 -0500

    use as week 3 HW and add a switch and potmeter to control the conten of HTML, connected with arduino

--- clientScript.js
+++ clientScript.js
@@ -1,5 +1,5 @@
-const socket = new WebSocket("ws://localhost:1880/testPage");
-
+//const socket = new WebSocket("ws://localhost:1880/testPage");
+const socket = new WebSocket("ws://localhost:1880/week3_HW");
 // socket.addEventListener("open", handleSocketOpen);
 
 // function handleSocketOpen(){
@@ -8,8 +8,26 @@ const socket = new WebSocket("ws://localhost:1880/testPage");
 
 socket.addEventListener("message", handleMessage);
 
-function handleMessage(event){
-        //document.body.style.backgroundColor= "green"
-        document.getElementById("socket_data").innerHTML=event.data;
-       // document.body.style.backgroundColor = "rgb(" + event.data*255 + ",0,0)";
-    };
+    function handleMessage(event){
+        const data = event.data.split(",");
+        const analogValue = parseInt(data[0]);
+        const switchState = parseInt(data[1]);
+        const r = Math.round(Math.random() * 255);
+
+    
+            //document.body.style.backgroundColor= "green"
+            //document.getElementById("socket_data").innerHTML=event.data;
+            //document.getElementById("socket_data").innerHTML=analogValue;
+            if (switchState == 1) {
+                document.getElementById("socket_data").innerHTML= "Hello";
+        } else {
+                document.getElementById("socket_data").innerHTML="";
+        }
+        const colorIntensity = Math.round(analogValue / 1023 * 255);
+        document.body.style.backgroundColor = `rgb(${colorIntensity}, ${r}, ${r})`;
+    
+            // Update HTML content with analog value
+            document.getElementById("analogValue").innerText = `Analog value: ${analogValue}`;
+        
+           // document.body.style.backgroundColor = "rgb(" + event.data*255 + ",0,0)";
+        };
